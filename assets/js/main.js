550456bba32a591f84ce8dc96acd27483253865e
TaylAmber <taylamber352@gmail.com>
2024-12-25 10:14:48 +0500

    CGPA Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,26 @@
-AOS.init();
+document.addEventListener('DOMContentLoaded', function() {
+  AOS.init({
+      offset: 120,
+      delay: 0,
+      duration: 1300,
+      easing: "ease",
+      once: false,
+      mirror: true,
+      anchorPlacement: "top-bottom",
+  });
+//? What are your main objectives!! 
+//! You want that!
+//* Are you sure:(
 
-// You can also pass an optional settings object
-// below listed default settings
-AOS.init({
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 1300, // values from 0 to 3000, with step 50ms
-  easing: "ease", // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+  // Reinitialize AOS on page navigation or other dynamic content loads
+  document.querySelectorAll('.nav-link').forEach(link => {
+      link.addEventListener('click', function() {
+          setTimeout(function() {
+              AOS.refresh(); // Reinitialize AOS
+          }, 500); // Adjust timeout as needed for your content loading speed
+      });
+  });
 });
-
 // main.js
 // main.js
 
